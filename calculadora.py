9928e9a6798216a41593297a2760c1f2875c9aa3
Lisandro <Lisandro@Desktop.pt>
2026-03-05 00:21:08 -0300

    especificacao do exercicio da calculadora

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+# Develop a calculator with the operations: add, subtract, divide and multiply
+# The calculator should work this way:
+# 1 - Receive an input from user specifying wich operation the calculator should perform:
+#      1 - add
+#      2 - subtract
+#      3 - divide
+#      4 - multiply
+# Note: You should validate that the user passed only an available option, returning an error in case
+#       He passed any unavaible option. If user pass an unvailable option, run this: raise Exception("Opcao invalida, escolha um numero de 1-4")
+# 2 - Require two numbers from user which will be stored on variables 'a' and 'b' respectively
+# 3 - According to users inputs (operation and parameters) perform the required operation and print the result
+
